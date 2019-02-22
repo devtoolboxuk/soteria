bcfb25fe50c7faa470a82042dd2b9f916dddcd65
Rob Wilson <rob.wilson@supergroup.co.uk>
2019-02-22 08:57:26 +0000

    Updated with some UTF8 and 7Bit itemes

--- src/Handlers/XssHandler.php
+++ src/Handlers/XssHandler.php
@@ -5,6 +5,7 @@ namespace soteria\secure\handlers;
 use soteria\secure\Wrappers\AttributeWrapper;
 use soteria\secure\Wrappers\HtmlWrapper;
 use soteria\secure\Wrappers\JavaScriptWrapper;
+use soteria\secure\Wrappers\UtfWrapper;
 use soteria\secure\Wrappers\XssFinalise;
 use soteria\secure\Wrappers\XssInitialise;
 
@@ -15,6 +16,7 @@ class XssHandler extends Handler
         parent::__construct($value);
         $this->setPassArray($passArray);
         $this->pushWrapper(new XssInitialise());
+        $this->pushWrapper(new UtfWrapper());
         $this->pushWrapper(new JavaScriptWrapper());
         $this->pushWrapper(new AttributeWrapper());
         $this->pushWrapper(new HtmlWrapper());
---
+++ src/Wrappers/UtfWrapper.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace soteria\secure\Wrappers;
+
+class UtfWrapper extends Wrapper
+{
+
+    private $name;
+
+    function getWrapperName()
+    {
+        $this->name = str_replace(__NAMESPACE__ . "\\", "", __CLASS__);
+        return $this->name;
+    }
+
+
+    public function process()
+    {
+
+        $value = $this->getValue();
+
+        #7 bit
+        $value = preg_replace('/[\x00-\x1F\x7F-\xFF]/', $this->getReplacementValue(), $value);
+
+        #8 bit
+        $value = preg_replace('/[\x00-\x1F\x7F]/', $this->getReplacementValue(), $value);
+
+        #UTF 8
+        $value = preg_replace('/[\x00-\x1F\x7F]/u', $this->getReplacementValue(), $value);
+
+
+        $this->setValue($value);
+    }
+
+}
